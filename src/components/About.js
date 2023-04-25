f88e4e103f6f7ef5cf83c50485ed8e7274645eaf
Prosun Gupta <83406066+progxy75@users.noreply.github.com>
2023-04-26 02:22:32 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+import AboutImg from "../assets/about-img.png";
+
+const About = () => {
+  return (
+    <section className="bg-secondery text-white px-5 py-32" id="about">
+      <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
+        <div className="about-info">
+          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
+            About Me
+          </h2>
+
+          <p className="pb-5">
+            Hi, My Name is Amit Rana. I am a college student &
+            Frontend Development practitioner. I build beautifull websites with React and
+            Tailwind CSS.
+          </p>
+          <p className="pb-5">
+            I am proficient in Frontend skills like React.js, Redux, Tailwind CSS, SaSS, Css3, etc.
+          </p>
+
+          <p>In backend I know Node.js, learning MongoDB</p>
+
+          <p>
+            In my spare time I create YouTube videos and write blogs on my Blog.
+            Where I talk about programming theory and build various projects.
+          </p>
+        </div>
+
+        <div className="about-img">
+          <img
+            src={AboutImg}
+            alt="coding illustration"
+            className="lgw-[80%] md:ml-auto"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="py-4 text-center bg-primary text-white "> &copy; 2023 AmitRana all right reserved</div>;
+};
+
+export default Footer;
